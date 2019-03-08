b930c97f69f2ecd3cb881b1547cf2658077d4097
MANIKANDAN1601 <48342764+MANIKANDAN1601@users.noreply.github.com>
2019-03-08 10:06:33 +0530

    Update km.php

--- km.php
+++ km.php
@@ -3,7 +3,7 @@
 	echo "<table border=2>";
 	echo "<tr>";
 	echo "<th>Entryname</th>";
-	echo "<th align=\"centre\">size</th>";
+	echo "<th align=\"centre\">size1</th>";
 	echo "<tr>\n";
 	while($entryname=readdir($mydirectory))
 	{
